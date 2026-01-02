7ce0a2383725e377d378678d5045bd40092bc85e
Suman Sourabh <sumansourabhcse@gmail.com>
2026-01-02 06:13:45 +0100

    Create layout.py

---
+++ utils/layout.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+def load_header():
+    st.markdown("""
+        <h1 style='text-align:center;'>📊 Investment Planner Dashboard</h1>
+        <hr>
+    """, unsafe_allow_html=True)
